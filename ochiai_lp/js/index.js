94c619aab234b0c02e8b8f6629e4923e37bf1b19
Kento <likelivefunfunny1994@gmail.com>
2020-05-25 18:42:23 +0900

    表示/非表示の切り替え実装

--- ochiai_lp/js/index.js
+++ ochiai_lp/js/index.js
@@ -2,18 +2,16 @@
 function displayChange (indexNo) {
     // 表示処理のターゲットとなる説明テキスト要素を取得。配列として返ってくる。
     var targetElements = this.document.getElementsByClassName("timeline-sentense");
+
+    // 複数要素の配列から引数をインデックスにしてターゲットを取得する
+    var target = targetElement[indexNo];
+
     // 引数のindexNoを配列のインデックスとして使用し、表示設定を変更する。
-    targetElement[indexNo].style.display = "block";
+    if (target.style.display == "block") {
+        // 既に対象エリアが表示されている状態でタイトル押下の場合、非表示
+        target.style.display = "none"; 
+    }else if (target.style.display == "none") {
+        // 非表示の状態でタイトル押下の場合、表示
+        target.style.display = "block";
+    }
 }
-
-
-// window.onload = function () {
-    // // ページ読み込み後に年表コンテンツの説明テキストを非表示にする処理を行う
-    // var targetElement = this.document.getElementsByClassName("timeline-sentense");
-// 
-//     // 取得した説明テキスト要素は複数あるので、一つずつ要素を非表示させるループ
-//     // ここで非表示にした説明テキストを、年表のタイトル押下時に表示させる
-//     for (var i = 0; i < targetElement.length; i++) {
-//         targetElement[i].style.display = "none";
-//     }
-// };
