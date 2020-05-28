78b5542750079f4fd4cb50426136b857301b4902
Kento <likelivefunfunny1994@gmail.com>
2020-05-28 15:50:05 +0900

    タイムラインの表示/非表示の機能の調整

--- ochiai_lp/js/index.js
+++ ochiai_lp/js/index.js
@@ -4,14 +4,17 @@ function displayChange (indexNo) {
     var targetElements = this.document.getElementsByClassName("timeline-sentense");
 
     // 複数要素の配列から引数をインデックスにしてターゲットを取得する
-    var target = targetElement[indexNo];
+    var target = targetElements[indexNo];
 
-    // 引数のindexNoを配列のインデックスとして使用し、表示設定を変更する。
-    if (target.style.display == "block") {
-        // 既に対象エリアが表示されている状態でタイトル押下の場合、非表示
-        target.style.display = "none"; 
-    }else if (target.style.display == "none") {
-        // 非表示の状態でタイトル押下の場合、表示
-        target.style.display = "block";
-    }
+    // // classListのtoggleでクラス属性値の切り替えを行う
+    target.classList.toggle("show");
+
+    // // 引数のindexNoを配列のインデックスとして使用し、表示設定を変更する。
+    // if (target.className == "timeline-sentense") {
+    //     // 既に対象エリアが表示されている状態でタイトル押下の場合、非表示
+    //     target.className = "timeline-sentense-show";
+    // }else if (target.className == "timeline-sentense-show") {
+    //     // 非表示の状態でタイトル押下の場合、表示
+    //     target.className = "timeline-sentense";
+    // }
 }
